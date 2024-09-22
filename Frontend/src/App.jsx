0b0198d3ac6710cb1332e67479695f824a94605d
atifali78 <atif7803320@gmail.com>
2024-09-22 13:59:24 +0500

    import all the components into App jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
+import React from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { browserRouter as Router } from "React-router-dom";
 import "./app.css";
-  
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import WorkoutSessions from "./components/WorkoutSessions";
+import Gallery from "./components/Gallery";
+import Pricing from "./components/Pricing";
+import BMICalculator from "./components/BMICalculator";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 export const App = () => {
-  return <Router>
-  
-    <Navbar />
-    <Hero />
-    <WorkoutSessions />
-    <Gallery />
-    <Pricing />
-    <Contact />
-    <BMICalculator />
-    <Footer />
-    <ToastContainer theme='dark' position='top-center'/>
-    
-  </Router>;
-}
+  return (
+    <Router>
+      <Navbar />
+      <Hero />
+      <WorkoutSessions />
+      <Gallery />
+      <Pricing />
+      <Contact />
+      <BMICalculator />
+      <Footer />
+      <ToastContainer theme="dark" position="top-center" />
+    </Router>
+  );
+};
 export default App;
